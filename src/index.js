5bf0f3845280fb5f958691b55badbf4dcd865874
Sam Landfried <samlandfried@gmail.com>
2018-09-16 22:53:00 -0600

    Add a hint of project organization
    Breaking up these 2 files separates concerns. index.js can focus on
    starting the app, and server.js can focus on handling network traffic.
    
    Also, creating a file that exports something gives us a nice bit of code
    to test =)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import express from 'express'
-const app = express()
+import server from './api/server'
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
